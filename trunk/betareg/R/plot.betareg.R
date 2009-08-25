6699f4bb08ffeb4173b485a2063d580cb2bff32c
zeileis <zeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-25 01:50:49 +0000

    complete re-write of betareg
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/betareg@23 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/betareg/R/plot.betareg.R
+++ trunk/betareg/R/plot.betareg.R
@@ -1,57 +1,122 @@
-"plot.betareg" <-
-function (x, which = 1:4, caption = c("Deviance residuals vs indices of obs.", 
-    "Standardized residuals vs indices of obs.", "Generalized leverage vs. Predicted values", "Cook's distance plot"), 
-    panel = points, sub.caption = deparse(x$call), main = "", 
+plot.betareg <- function(x, which = 1:4,
+  caption = c("Pearson residuals vs indices of obs.", "Deviance residuals vs indices of obs.",
+    "Cook's distance plot", "Generalized leverage vs predicted values",
+    "Half-normal plot of standardized residuals", "Half-normal plot of deviance residuals",
+    "Pearson residuals vs linear predictor", "Deviance residuals vs linear predictor"),
+    sub.caption = paste(deparse(x$call), collapse = "\n"), main = "", 
     ask = prod(par("mfcol")) < length(which) && dev.interactive(), 
-    ..., id.n = 3, labels.id = names(residuals(x)), cex.id = 0.75) 
+    ..., nsim = 100, level = 0.9)
 {
-    if (!inherits(x, "betareg")) 
-        stop("Use only with 'betareg' objects")
-    if (!is.numeric(which) || any(which < 1) || any(which > 4)) 
-        stop("`which' must be in 1:4")
-    rdev <- residuals(x,type="deviance")
-    n <- length(rdev)
-    h <- x$h
-    k <- x$k
-    gl <- x$GL
-    gl <- diag(gl)
-    yh <- predict(x)
-    sr <- residuals(x)
-    show <- rep(FALSE, 4)
-    show[which] <- TRUE
-    one.fig <- prod(par("mfcol")) == 1
-    if (ask) {
-        op <- par(ask = TRUE)
-        on.exit(par(op))
-    }
-    if (show[1]) {
-        plot(rdev, xlab = "Indices of obs.", ylab = "Deviance residuals", main = main, ...)
-        if (one.fig) 
-            title(sub = sub.caption, ...)
-        mtext(caption[1], 3, 0.25)
-        abline(h = 0, lty = 3, col = "gray")
-    }
-    if (show[2]) {
-        plot(sr, xlab = "Obs. number", ylab = "Standardized residuals", main = main, ...)
-        if (one.fig) 
-            title(sub = sub.caption, ...)
-        mtext(caption[2], 3, 0.25)
-        abline(h = 0, lty = 3, col = "gray")
-    }
-    if (show[3]) {
-	        plot(yh, gl, xlab = "Fitted values", ylab = "Generalized leverage", main = main,...)
-        panel(yh, gl, ...)
-        if (one.fig) 
-            title(sub = sub.caption, ...)
-        mtext(caption[3], 3, 0.25)
-    }
-    if (show[4]) {
-	plot(h*(sr^2)/(k*(1-h)^2),xlab="Obs. number",ylab = "Cook's distance",type="h",main=main)
-	mtext(caption[4], 3, 0.25)
-       if (one.fig) 
-            title(sub = sub.caption, ...)
-    }
-    if (!one.fig && par("oma")[3] >= 1) 
-        mtext(sub.caption, outer = TRUE, cex = 1.25)
-    invisible()
+  if(!is.numeric(which) || any(which < 1) || any(which > 8)) 
+    stop("`which' must be in 1:8")
+  rd <- residuals(x, type = "deviance")
+  rp <- residuals(x, type = "pearson")
+  n <- length(rd)
+  k <- length(x$coefficients) - 1
+  show <- rep(FALSE, 8)
+  show[which] <- TRUE
+  one.fig <- prod(par("mfcol")) == 1
+  if(ask) {
+    op <- par(ask = TRUE)
+    on.exit(par(op))
+  }
+  if(show[1]) {
+    plot(1:n, rp, xlab = "Obs. number", ylab = "Pearson residuals", main = main, ...)
+    if(one.fig) title(sub = sub.caption, ...)
+    mtext(caption[1], 3, 0.25)
+    abline(h = 0, lty = 3, col = "gray")
+  }
+  if(show[2]) {
+    plot(1:n, rd, xlab = "Indices of obs.", ylab = "Deviance residuals", main = main, ...)
+    if(one.fig) title(sub = sub.caption, ...)
+    mtext(caption[2], 3, 0.25)
+    abline(h = 0, lty = 3, col = "gray")
+  }
+  if(show[3]) {
+    plot(1:n, cooks.distance(x),
+      xlab = "Obs. number", ylab = "Cook's distance", type = "h", main = main)
+    if(one.fig) title(sub = sub.caption, ...)
+    mtext(caption[3], 3, 0.25)
+  }
+  if(show[4]) {
+    plot(fitted(x), gleverage(x),
+      xlab = "Predicted values", ylab = "Generalized leverage", main = main, ...)
+    if(one.fig) title(sub = sub.caption, ...)
+    mtext(caption[4], 3, 0.25)
+  }
+  if(show[5]) {
+    hn <- halfnormal.betareg(x, nsim = nsim, level = level, type = "pearson")
+    plot(hn[,1], hn[,2], ylim = range(hn[,-1]), main = main,
+      xlab = "Normal quantiles", ylab = "Absolute values of Pearson residuals", ...)
+    lines(hn[,1], hn[,3],lty = 2)
+    lines(hn[,1], hn[,4],lty = 1)
+    lines(hn[,1], hn[,5],lty = 1)
+    if(one.fig) title(sub = sub.caption, ...)
+    mtext(caption[5], 3, 0.25)
+  }
+  if(show[6]) {
+    hn <- halfnormal.betareg(x, nsim = nsim, level = level, type = "deviance")
+    plot(hn[,1], hn[,2], ylim = range(hn[,-1]), main = main,
+      xlab = "Normal quantiles", ylab = "Absolute values of deviance residuals", ...)
+    lines(hn[,1], hn[,3],lty = 2)
+    lines(hn[,1], hn[,4],lty = 1)
+    lines(hn[,1], hn[,5],lty = 1)
+    if(one.fig) title(sub = sub.caption, ...)
+    mtext(caption[6], 3, 0.25)
+  }
+  if(show[7]) {
+    plot(predict(x, type = "link"), rp,
+      xlab = "Linear predictor", ylab = "Pearson residuals", main = main, ...)
+    if(one.fig) title(sub = sub.caption, ...)
+    mtext(caption[7], 3, 0.25)
+    abline(h = 0, lty = 3, col = "gray")
+  }
+  if(show[8]) {
+    plot(predict(x, type = "link"), rd,
+      xlab = "Linear predictor", ylab = "Deviance residuals", main = main, ...)
+    if(one.fig) title(sub = sub.caption, ...)
+    mtext(caption[8], 3, 0.25)
+    abline(h = 0, lty = 3, col = "gray")
+  }
+
+  if(!one.fig && par("oma")[3] >= 1) mtext(sub.caption, outer = TRUE, cex = 1.25)
+  invisible()
+}
+
+halfnormal.betareg <- function(model, nsim = 100, level = 0.90, type = c("pearson", "deviance"))
+{
+  type <- match.arg(type)
+
+  ## extract response y and regressors X
+  y <- if(is.null(model$y)) model.response(model.frame(model)) else model$y
+  x <- if(is.null(model$x)) model.matrix(model) else model$x
+  offset <- if(is.null(model$offset)) rep(0, NROW(x)) else model$offset
+  wts <- weights(model)
+
+  n <- NROW(x)
+  alpha <- (1 - level)/2
+  mu <- fitted(model)
+  phi <- tail(model$coefficients, 1)    
+  res <- residuals(model, type = type)
+
+  e <- matrix(0, n, nsim)
+  e1 <- numeric(n)
+  e2 <- numeric(n)
+  
+  for(i in 1:nsim) {
+    ysim <- rbeta(n, mu * phi, (1 - mu) * phi)
+    fit <- betareg(ysim ~ 0 + x, weights = wts, offset = offset)
+    e[,i] <- sort(abs(residuals(fit, type = type)))
+  }
+  
+  for(i in 1:n) {
+    eo <- sort(e[i,])
+    e1[i] <- quantile(eo, alpha)
+    e2[i] <- quantile(eo, 1 - alpha)
+  }
+  
+  e0 <- apply(e, 1, median)
+  qq <- qnorm((n + 1:n + 0.5)/(2 * n + 1.125))
+  
+  cbind(qq, sort(abs(res)), e0, e1, e2)  
 }
