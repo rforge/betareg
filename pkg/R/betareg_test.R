23dc08d164531875914931078146d1cdc57065a3
ioannis <ioannis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-04 00:38:30 +0000

    Added betareg_test.R which includes some fits that should be identical to the ones reported in Kosmidis & Firth (2010, EJS)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/betareg@118 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/betareg_test.R
@@ -0,0 +1,32 @@
+rm(list = ls())
+library(betareg)
+source("betareg.R")
+source("make.dmu.deta.R")
+data("GasolineYield", package = "betareg")
+
+## Same results as in Table 1 in Kosmidis and Firth (2010, EJS,
+## http://dx.doi.org/10.1214/10-EJS579)
+gyML <- betareg(yield ~ batch + temp, data = GasolineYield, link.phi = "identity")
+gyBC <- betareg(yield ~ batch + temp, data = GasolineYield, link.phi = "identity",
+                type = "BC")
+gyBR <- betareg(yield ~ batch + temp, data = GasolineYield, link.phi = "identity",
+                type = "BR")
+
+
+## Same results as in Table 3 in Kosmidis and Firth (2010, EJS,
+## http://dx.doi.org/10.1214/10-EJS579). BR and BC estimates in the
+## latter study were calculated in a different way than betareg
+## computes them which provides some indication on the correctness of
+## implementation
+gyMLlog <- betareg(yield ~ batch + temp, data = GasolineYield, link.phi = "log")
+gyBClog <- betareg(yield ~ batch + temp, data = GasolineYield, link.phi = "log",
+                   type = "BC")
+gyBRlog <- betareg(yield ~ batch + temp, data = GasolineYield, link.phi = "log",
+                   type = "BR")
+
+
+## Fit with temp as a dispersion covariate
+gy2ML <- betareg(yield ~ batch + temp | temp, data = GasolineYield)
+gy2BC <- betareg(yield ~ batch + temp | temp, data = GasolineYield, type = "BC")
+gy2BR <- betareg(yield ~ batch + temp | temp, data = GasolineYield, type = "BR")
+
