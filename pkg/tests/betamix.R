759a67b22a56fb87f3b1ecc03b2cbb882d0e8af2
gruen <gruen@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-19 13:36:51 +0000

    .save files for vignettes and Examples added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/betareg@152 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/betamix.R
+++ pkg/tests/betamix.R
@@ -3,6 +3,7 @@ library("betareg")
 data("ReadingSkills", package = "betareg")
 
 ## beta regression mixture model
+set.seed(1071)
 rs_mix <- betamix(accuracy ~ iq, data = ReadingSkills, k = 3,
   extra_components = extraComponent(type = "uniform",
     coef = 0.99, delta = 0.01), nstart = 10)
